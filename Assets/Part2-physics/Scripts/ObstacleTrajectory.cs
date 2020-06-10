7961e57d3fe039b706175a7ef2148edf9ea663c3
Samuel Bouchet <contact@samuel-bouchet.fr>
2020-06-10 08:30:44 +0200

    Fix moving obstacle collision force

--- Assets/Part2-physics/Scripts/ObstacleTrajectory.cs
+++ Assets/Part2-physics/Scripts/ObstacleTrajectory.cs
@@ -12,13 +12,18 @@ public class ObstacleTrajectory : MonoBehaviour {
     }
 
     private Vector2 lastPos = Vector2.zero;
-    void Update() {
+
+    // Utilise FixedUpdate pour manipuler la physique
+    void FixedUpdate() {
+        // calcule de la prochaine position
         lastPos = rb.position;
         rb.position = new Vector3(
             Mathf.Cos((Offset + Time.time * Speed) * Mathf.PI * 2) * Radius,
             Mathf.Sin((Offset + Time.time * Speed) * Mathf.PI * 2) * Radius,
             0
         );
+        // astuce pour avoir un calcule de vélocité qui impacte la physique : calcule une vélocité et repositionne le kinematic à sa dernière position
         rb.velocity = (rb.position - lastPos) / Time.deltaTime;
+        rb.position = lastPos;
     }
 }
